fee412e128e24c00d9c4aaf34126ac36c24bb666
bruce.kung <775100939@qq.com>
2019-11-08 17:16:50 +0800

    登录页面

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,40 +1,122 @@
 import React, {Component, Fragment} from 'react';
 import {Nav, Container} from '../../components/Common';
-import {List, InputItem} from "antd-mobile";
-import {createForm} from 'rc-form';
+import {List, InputItem, Flex, Checkbox} from "antd-mobile";
+import {createForm, formShape} from 'rc-form';
+import Images from '../../utils/image';
 import './index.scss';
 
-@createForm()
+const AgreeItem = Checkbox.AgreeItem;
+
+class Password extends Component {
+  static propTypes = {
+    form: formShape,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      eyeStatus: undefined,//眼睛闭
+    }
+  }
+
+  onChange = (e) => {
+    this.setState({
+      value: e.target.value
+    })
+  };
+  onClickEye = () => {
+    const password = this.props.form.getFieldInstance('password');
+    this.setState((prevState, props) => {
+      const eyeStatus = prevState.eyeStatus;
+      let type;
+      if (eyeStatus === undefined || eyeStatus === false) {
+        type = 'text';
+      } else if (eyeStatus === true) {
+        type = 'password';
+      }
+      password.type = type;
+      return {
+        eyeStatus: !eyeStatus
+      };
+    });
+  };
+
+  render() {
+    const {value, eyeStatus} = this.state;
+    let imgSrc;
+    if (eyeStatus === undefined || eyeStatus === false) {
+      imgSrc = Images.eyeClosed;
+    } else if (eyeStatus === true) {
+      imgSrc = Images.eyeOpened;
+    }
+    const {getFieldProps, getFieldDecorator, getFieldError, isFieldValidating} = this.props.form;
+    return (
+      <Flex className="input-item">
+        <span className="ipt">
+          <input type="password"
+                 placeholder="请输入密码"
+                 {...getFieldProps('password', {
+                   initialValue: value,
+                   onChange: this.onChange,
+                   rules: [
+                     {required: true}
+                   ]
+                 })}/>
+          {/*{getFieldDecorator('password', {
+            initialValue: {abc},
+            onChange: this.onChange,
+            rules: [
+              {required: true}
+            ]
+          })(<input type="text" placeholder="请输入密码"/>)}*/}
+        </span>
+        <span className="img">
+          {
+            value.length > 0 && <img src={imgSrc} onClick={this.onClickEye}/>
+          }
+          </span>
+      </Flex>)
+  }
+}
+
+
 class Index extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    const {getFieldProps} = this.props.form;
+    const {form} = this.props
+    const {getFieldDecorator} = form;
     return (
       <Fragment>
         <Nav title="登录" onLeftClick={() => this.props.history.goBack()}/>
         <Container>
-            <InputItem
-              {...getFieldProps('input3')}
-              placeholder="no label"
-            />
-            <InputItem
-              {...getFieldProps('inputtitle2')}
-              placeholder="title can be icon，image or text"
-            >
-              <div style={{
-                backgroundImage: 'url(https://zos.alipayobjects.com/rmsportal/DfkJHaJGgMghpXdqNaKF.png)',
-                backgroundSize: 'cover',
-                height: '22px',
-                width: '22px'
-              }}/>
-            </InputItem>
+          <div className="login-sec">
+            <form onSubmit={this.onSubmit}>
+              <Flex className="input-item">
+                <span className="ipt">
+                  {getFieldDecorator('userName', {
+                    rules: [
+                      {required: true}
+                    ]
+                  })(<input type="text" placeholder="请输入用户名/手机号码"/>)}
+                </span>
+              </Flex>
+              <Password form={form}/>
+              <Flex className="auto-login" justify="between">
+                <AgreeItem className="auto-lg" onChange={e => console.log('checkbox', e)}>
+                  自动登录
+                </AgreeItem>
+                <span className="forgot">忘记密码?</span>
+              </Flex>
+            </form>
+          </div>
         </Container>
       </Fragment>
     );
   }
 }
 
-export default Index;
+export default createForm()(Index);
